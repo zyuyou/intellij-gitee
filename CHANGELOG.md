b4580f9700928d157eb598522d54c435fb5c8fe7
zyuyou <zyuyou@163.com>
2022-04-06 23:00:43 +0800

    1.迁移gradle构建脚本为kotlin脚本; 2.使用gradle-changelog-plugin构建changeNotes;

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
-## Change log
+## [Unreleased]
+- Compatible with idea-2022.1
+- Remove some deprecated api
+- Fix task issue info request error
 
-### Version 2018.3.0.preview
-* 适配IDEA的VCS.GIT新框架
-* 重构代码, 采用`gitee`作为码云统一标识, 修改代码包名为码云域名
+## [2022.1.0]
+- Compatible with idea-2022.1
+- Remove some deprecated api
+- Fix task issue info request error
---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+import org.jetbrains.changelog.date
+
+fun properties(key: String) = project.findProperty(key)?.toString()
+
+plugins {
+  id("java")
+  id("org.jetbrains.kotlin.jvm") version "1.6.20"
+  id("org.jetbrains.intellij") version "1.5.2"
+  id("org.jetbrains.changelog") version "1.3.1"
+}
+
+group = "com.gitee"
+version = "${properties("pluginVersion")}.${properties("pluginBuildNumber")}"
+
+repositories {
+  mavenCentral()
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
+}
+
+configurations {
+  implementation {
+    resolutionStrategy.failOnVersionConflict()
+  }
+}
+
+intellij {
+  version.set(properties("ideaVersion"))
+
+  pluginName.set("intellij-gitee")
+  plugins.set(listOf("tasks", "git4idea"))
+
+  downloadSources.set(System.getenv("CI_BUILD").toBoolean())
+
+}
+
+tasks {
+  buildSearchableOptions {
+    enabled = false
+  }
+
+  publishPlugin {
+    token.set(properties("publishToken"))
+    channels.set(listOf("stable"))
+  }
+
+  patchPluginXml {
+    version.set("${project.version}")
+    sinceBuild.set(properties("ideaBuildVersion"))
+    untilBuild.set("${properties("ideaBuildVersion")}.*")
+    changeNotes.set(
+      provider { changelog.getLatest().toHTML() }
+    )
+  }
+
+  compileKotlin {
+    kotlinOptions.jvmTarget = "11"
+  }
+
+  compileTestKotlin {
+    kotlinOptions.jvmTarget = "11"
+  }
+}
+
+changelog {
+  version.set("${project.version}")
+  path.set("${project.projectDir}/CHANGELOG.md")
+  header.set(provider { "[${version.get()}] - ${date()}" })
+  itemPrefix.set("-")
+  keepUnreleasedSection.set(true)
+  unreleasedTerm.set("[Unreleased]")
+}
