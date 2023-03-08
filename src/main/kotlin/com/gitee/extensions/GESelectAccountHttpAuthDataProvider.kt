5f1acf27312c980c0e1136f0d020afd300e7b88f
zyuyou <zyuyou@163.com>
2023-03-08 12:21:52 +0800

    fix refresh tokens error when multi accounts.

--- src/main/kotlin/com/gitee/extensions/GESelectAccountHttpAuthDataProvider.kt
+++ src/main/kotlin/com/gitee/extensions/GESelectAccountHttpAuthDataProvider.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.gitee.extensions
 
-import com.gitee.authentication.GEAccountAuthData
 import com.gitee.authentication.GEAccountsUtil
 import com.gitee.authentication.GECredentials
 import com.gitee.authentication.accounts.GiteeAccount
@@ -26,11 +25,13 @@ internal class GESelectAccountHttpAuthDataProvider(
     val credentials = potentialAccounts[account]
       ?: GEAccountsUtil.requestNewCredentials(account, project, parentComponent)
       ?: return null
+
     if (setDefault) {
       GEAccountsUtil.setDefaultAccount(project, account)
     }
 
-    return GEAccountAuthData(account, account.name, credentials)
+    return GEAccountsUtil.requestReLogin(account, credentials, project)
+//    return GEAccountAuthData(account, account.name, credentials)
   }
 
   private fun chooseAccount(parentComponent: Component?): Pair<GiteeAccount, Boolean>? {
